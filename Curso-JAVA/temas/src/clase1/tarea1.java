68ad15ed44fe5ff5e50f4b7c6dbbcd56c8c31100
crisguz93 <cristiang_93@hotmail.com>
2023-04-08 19:03:36 -0300

    tarea1

---
+++ Curso-JAVA/temas/src/clase1/tarea1.java
@@ -0,0 +1,34 @@
+package clase1;
+
+public class tarea1 {
+    public static void main(String[] args) {
+        /*numeros*/
+        byte n1= 4;
+        short n2= 20;
+        int n3= 6;
+        long n4= 50;
+        System.out.println("variable de byte: "+n1);
+        System.out.println("variable de short: "+n2);
+        System.out.println("variable de entero: "+n3);
+        System.out.println("variable de long: "+n4);
+
+        /*decimales*/
+        float n5= 4.5f;
+        double n6= 6.9;
+        System.out.println("variable de float: "+n5);
+        System.out.println("variable de double: "+n6);
+
+        /*booleanos*/
+        boolean n7= false;
+        boolean n8= true;
+
+        System.out.println("variable booleana: "+n7);
+        System.out.println("variable booleana: "+n8);
+
+        /*texto*/
+        char n9= 'F';
+        String n10= "me llamo Cristian";
+        System.out.println("variable de caracter: "+n9);
+        System.out.println("variable de cadena: "+n10);
+    }
+}
